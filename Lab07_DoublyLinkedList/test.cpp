d2bcf76bdf90716fe2e4d082040e32c306fe878c
AnooshaaAli <155889714+AnooshaaAli@users.noreply.github.com>
2024-07-04 12:30:10 +0500

    Create test.cpp

---
+++ Lab07_DoublyLinkedList/test.cpp
@@ -0,0 +1,254 @@
+#include "pch.h"
+#include "main.h"
+
+
+//***************** Q1 Test cases ********************
+TEST(DoublyLinkedListTest, EmptyList) {
+    DoublyLinkedList dll;
+    EXPECT_TRUE(dll.isEmpty());
+}
+
+TEST(DoublyLinkedListTest, InsertToHead) {
+    DoublyLinkedList dll;
+    dll.insertToHead(10);
+    dll.insertToHead(20);
+    dll.print();
+    int expected[] = { 20, 10 };
+    int result[2] = { 0 };
+
+    int i = 0;
+    DoublyNode* current = dll.getHead();
+    while (current) {
+        result[i++] = current->getData();
+        current = current->getNext();
+    }
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+TEST(DoublyLinkedListTest, InsertToEnd) {
+    DoublyLinkedList dll;
+    dll.insert(10);
+    dll.insert(20);
+    dll.print();
+    int expected[] = { 10, 20 };
+    int result[2] = { 0 };
+
+    int i = 0;
+    DoublyNode* current = dll.getHead();
+    while (current) {
+        result[i++] = current->getData();
+        current = current->getNext();
+    }
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+TEST(DoublyLinkedListTest, Search) {
+    DoublyLinkedList dll;
+    dll.insert(10);
+    dll.insert(20);
+    dll.insert(30);
+
+    dll.print();
+    EXPECT_TRUE(dll.search(20));
+    EXPECT_FALSE(dll.search(40));
+}
+
+TEST(DoublyLinkedListTest, Update) {
+    DoublyLinkedList dll;
+    dll.insert(10);
+    dll.insert(20);
+    dll.insert(30);
+
+    dll.update(20, 25);
+    dll.print();
+
+    EXPECT_TRUE(dll.search(25));
+    EXPECT_FALSE(dll.search(20));
+}
+
+TEST(DoublyLinkedListTest, InsertAtIndex) {
+    DoublyLinkedList dll;
+    dll.insert(10);
+    dll.insert(20);
+    dll.insert(40);
+
+    dll.insertAtIndex(30, 2);
+
+    dll.print();
+
+    int expected[] = { 10, 20, 30, 40 };
+    int result[4] = { 0 };
+
+    int i = 0;
+    DoublyNode* current = dll.getHead();
+    while (current) {
+        result[i++] = current->getData();
+        current = current->getNext();
+    }
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+TEST(DoublyLinkedListTest, Delete) {
+    DoublyLinkedList dll;
+    dll.insert(10);
+    dll.insert(20);
+    dll.insert(30);
+
+    dll.deleteData(20);
+
+    dll.print();
+
+    int expected[] = { 10, 30 };
+    int result[2] = { 0 };
+
+    int i = 0;
+    DoublyNode* current = dll.getHead();
+    while (current) {
+        result[i++] = current->getData();
+        current = current->getNext();
+    }
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+
+
+//****************** Q2 Test cases *******************
+TEST(CircularLinkedListTest, EmptyList) {
+    CircularLinkedList cll;
+    EXPECT_TRUE(cll.isEmpty());
+}
+
+TEST(CircularLinkedListTest, InsertToEnd) {
+    CircularLinkedList cll;
+    cll.insert(10);
+    cll.insert(20);
+
+    int expected[] = { 10, 20 };
+    int result[2] = { 0 };
+
+    cll.print();
+    int i = 0;
+    Node* current = cll.getHead();
+    do {
+        result[i++] = current->getData();
+        current = current->getNext();
+    } while (current != cll.getHead());
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+TEST(CircularLinkedListTest, Search) {
+    CircularLinkedList cll;
+    cll.insert(10);
+    cll.insert(20);
+    cll.insert(30);
+    cll.print();
+    EXPECT_TRUE(cll.search(20));
+    EXPECT_FALSE(cll.search(40));
+}
+
+TEST(CircularLinkedListTest, Update) {
+    CircularLinkedList cll;
+    cll.insert(10);
+    cll.insert(20);
+    cll.insert(30);
+
+    cll.update(20, 25);
+
+    cll.print();
+    EXPECT_TRUE(cll.search(25));
+    EXPECT_FALSE(cll.search(20));
+}
+
+TEST(CircularLinkedListTest, InsertAtIndex) {
+    CircularLinkedList cll;
+    cll.insert(10);
+    cll.insert(20);
+    cll.insert(40);
+
+    cll.insertAtIndex(30, 2);
+
+    cll.print();
+
+    int expected[] = { 10, 20, 30, 40 };
+    int result[4] = { 0 };
+
+    int i = 0;
+    Node* current = cll.getHead();
+    do {
+        result[i++] = current->getData();
+        current = current->getNext();
+    } while (current != cll.getHead());
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+TEST(CircularLinkedListTest, Delete) {
+    CircularLinkedList cll;
+    cll.insert(10);
+    cll.insert(20);
+    cll.insert(30);
+
+    cll.deleteData(20);
+    cll.print();
+    int expected[] = { 10, 30 };
+    int result[2] = { 0 };
+
+    int i = 0;
+    Node* current = cll.getHead();
+    do {
+        result[i++] = current->getData();
+        current = current->getNext();
+    } while (current != cll.getHead());
+
+    EXPECT_EQ(0, memcmp(expected, result, sizeof(expected)));
+}
+
+
+
+
+//**************** Q3 Test cases *******************
+
+TEST(CircularLinkedListTest, FindWinner) {
+    // Create a CircularLinkedList with skills [1, 2, 3, 4, 5]
+    CircularLinkedList cll;
+
+    cll.insert(1);
+    cll.insert(8);
+    cll.insert(7);
+    cll.insert(2);
+    cll.insert(5);
+    cll.print();
+    // Find the winner with M = 3 (eliminate every 3rd person)
+    int winnerSkill = cll.findWinner(3);
+
+    // The expected winner skill level is 4 (after eliminating 1, 2, 3).
+    EXPECT_EQ(winnerSkill, 8);
+}
+
+
+TEST(CircularLinkedListTest, FindWinner2) {
+    // Create a CircularLinkedList with skills [1, 2, 3, 4, 5]
+    CircularLinkedList cll;
+
+    cll.insert(1);
+    cll.insert(2);
+    cll.insert(7);
+    cll.insert(9);
+    cll.insert(15);
+    cll.print();
+    // Find the winner with M = 3 (eliminate every 3rd person)
+    int winnerSkill = cll.findWinner(3);
+
+    // The expected winner skill level is 4 (after eliminating 1, 2, 3).
+    EXPECT_EQ(winnerSkill, 15);
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
